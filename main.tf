18d9365456e4289d2b22cd75095db81675d00e06
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-21 15:24:54 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,44 @@
 	   
 */
 variable "policy_name"{
-  type = string
-  default = "LambdaAPIGatewayPolicy"
+  type 		= string
+  default 	= "LambdaAPIGatewayPolicy"
 }
+
+variable "role_name"{
+  type 		= string
+  default 	= "LambdaAPIGatewayRole"
+}
+
 
 resource "aws_iam_policy" "policy" {
   name          = var.policy_name
-  description   = "IAM Policy for Lambda Setup"
-
+  description   = "IAM Policy for Lambda Function"
   policy        = file("lambda_api_gw_policy.json")
-
   tags          = {
     Owner       = "dan-via-terraform"
   }
 }
+
+resource "aws_iam_role" "role" {
+  name			= var.role_name
+  description		= "IAM Role for Lambda Function"
+#  assume_role_policy 	= "${aws_iam_policy.policy.arn}"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+EOF
+}	
+	
+	
