ca8e888572bdc67b1dc8fb811469ae33f90c790d
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-22 17:39:27 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_lambda_function" "test_lambda" {
 	#environment
 }
 
-$ Create a rest api
+# Create a rest api
 resource "aws_api_gateway_rest_api" "created_api" {
   name = "DynamoDBOps"
 }
