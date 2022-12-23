9e87e7762328f9db18428de566c73cc6c99d7106
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-22 21:27:49 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,8 +100,8 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   integration_http_method = "POST"
   type        = "AWS_PROXY"   # lets API GW pass req to backend lambda function
   #uri 	      = aws_lambda_function.test_lambda.invoke_arn
-  #uri 	      = aws_lambda_function.test_lambda.arn
-  uri 	      = "arn:aws:lambda:us-west-2:500112433998:function:LambdaFunctionOverHttps"
+  uri 	      = aws_lambda_function.CreatedAPI.arn
+ #uri 	      = "arn:aws:lambda:us-west-2:500112433998:function:LambdaFunctionOverHttps"
 }
 
 
