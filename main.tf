bb1b6679b04fcae81c4079a331b523f3becc6567
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-22 19:53:16 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,7 +93,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   http_method = aws_api_gateway_method.http_post_method.http_method
   resource_id = aws_api_gateway_resource.created_resource.id
   rest_api_id = aws_api_gateway_rest_api.created_api.id
-  type        = "LAMBDA"
   type        = "AWS_PROXY"   # lets API GW pass req to backend lambda function
 }
 
