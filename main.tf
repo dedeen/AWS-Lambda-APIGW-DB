095453b13ca9f846410c135c3dea29ed3c7fd53c
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-26 00:22:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -117,15 +117,6 @@ resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
   resource_id = aws_api_gateway_resource.DynamoDBManager.id
   rest_api_id = aws_api_gateway_rest_api.CreatedAPI.id
   status_code = aws_api_gateway_method_response.response_200.status_code
-	
-  # Transforms the incoming XML request to JSON
-  request_templates = {
-    "application/xml" = <<EOF
-  {
-   "body" : $input.json('$')
-  }
-  EOF
-    }
 }
 
   
