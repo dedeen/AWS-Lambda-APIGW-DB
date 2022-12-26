aef023a4b26947cdfcb466d6f015f6968843609a
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-26 00:23:50 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ resource "aws_api_gateway_method_response" "response_200" {
   status_code = "200"
 }
 
-resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+resource "aws_api_gateway_integration_response" "integration_response" {
   http_method = aws_api_gateway_method.HTTPPostMethod.http_method
   resource_id = aws_api_gateway_resource.DynamoDBManager.id
   rest_api_id = aws_api_gateway_rest_api.CreatedAPI.id
