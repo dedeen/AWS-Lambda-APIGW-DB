c855179c9879b54cd53c8c66bbea371d1cfb9e90
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-26 00:21:33 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -118,10 +118,6 @@ resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
   rest_api_id = aws_api_gateway_rest_api.CreatedAPI.id
   status_code = aws_api_gateway_method_response.response_200.status_code
 	
-  request_parameters = {
-    "integration.request.header.X-Authorization" = "'static'"
-  }
-
   # Transforms the incoming XML request to JSON
   request_templates = {
     "application/xml" = <<EOF
