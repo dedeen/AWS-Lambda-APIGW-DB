29ea9a0404e19878ee2c8c76abd336a85706e4ae
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-26 14:09:14 -0600

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,1 +1,44 @@
-#
+console.log('Loading function2');
+
+var AWS = require('aws-sdk');
+var dynamo = new AWS.DynamoDB.DocumentClient();
+var tableName = "dyndb-table2";
+
+/**
+ * Provide an event that contains the following keys:
+ *
+ *   - operation: one of the operations in the switch statement below, or 'echo'
+ *   - payload: a parameter to pass to the operation being performed
+ */
+exports.handler = function(event, context, callback) {
+    
+    var operation = event.operation;
+
+    if (operation == 'echo'){
+        callback(null, event.payload);
+    }
+        
+    else{
+        event.payload.TableName = tableName;
+
+    switch (operation) {
+        case 'create':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'read':
+            dynamo.get(event.payload, callback);
+            break;
+        case 'update':
+            dynamo.update(event.payload, callback);
+            break;
+        case 'delete':
+            dynamo.delete(event.payload, callback);
+            break;
+        case 'list':
+            dynamo.scan(event.payload, callback);
+            break;
+        default:
+            callback(`Unknown operation: ${operation}`);
+    }
+}
+};
